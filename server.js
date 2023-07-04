ba4dd8e1cdec3f55b008db1feb323be992ec3621
Stephen Obialor <stevobialor@gmail.com>
2023-07-04 21:53:42 +0000

    create application server

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const routes = require('../routes/index');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use('/', routes);
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
