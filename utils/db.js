4e24ed5fac65a945c6bd655292f1f5ab1682823b
Stephen Obialor <stevobialor@gmail.com>
2023-07-04 20:38:15 +0000

    create the database connection instance via sequelize

---
+++ utils/db.js
@@ -0,0 +1,14 @@
+const Sequelize = require('sequelize');
+
+const dbName = process.env.BDC_DBNAME;
+const username = process.env.BDC_USERNAME;
+const password = process.env.BDC_PASSWORD;
+const host = process.DB_HOST || 'localhost';
+
+
+const dbConnect = new Sequelize(dbName, username, password, {
+  host,
+  dialect: 'mysql',
+});
+
+module.exports = dbConnect;
