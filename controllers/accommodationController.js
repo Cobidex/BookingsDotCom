e41b2140b6d536a821cde0b8cad74b8737a650be
DeazStar <deadfromtheborn@gmail.com>
2023-07-05 11:37:13 +0300

    add function for updating and deleteing accomodations

--- controllers/accommodationController.js
+++ controllers/accommodationController.js
@@ -50,7 +50,75 @@ const formatAccommodationForDisplay = (accommodation) => {
     return formattedAccommodation;
 };
 
+const updateAccommodation = async (req, res) => {
+    try {
+        const { id } = req.params; // Extract the accommodation ID from the request parameters
+
+        // Extract the updated data from the request body
+        const {
+            name,
+            description,
+            pricePerNight,
+            type,
+            availableDates,
+            cityId,
+            rating
+        } = req.body;
+
+        // Find the accommodation by ID
+        const accommodation = await Accommodation.findByPk(id);
+
+        // Check if the accommodation exists
+        if (!accommodation) {
+            return res.status(404).json({ error: 'Accommodation not found' });
+        }
+
+        // Update the accommodation with the new data
+        accommodation.name = name;
+        accommodation.description = description;
+        accommodation.pricePerNight = pricePerNight;
+        accommodation.type = type;
+        accommodation.availableDates = availableDates;
+        accommodation.cityId = cityId;
+        accommodation.rating = rating;
+
+        // Save the updated accommodation
+        await accommodation.save();
+
+        // Return the updated accommodation in the response
+        res.json(accommodation);
+    } catch (error) {
+        // Handle any errors that occur during the update process
+        res.status(500).json({ error: 'Failed to update accommodation' });
+    }
+};
+
+const deleteAccommodation = async (req, res) => {
+    try {
+        const { id } = req.params; // Extract the accommodation ID from the request parameters
+
+        // Find the accommodation by ID
+        const accommodation = await Accommodation.findByPk(id);
+
+        // Check if the accommodation exists
+        if (!accommodation) {
+            return res.status(404).json({ error: 'Accommodation not found' });
+        }
+
+        // Delete the accommodation
+        await accommodation.destroy();
+
+        // Return a success message in the response
+        res.json({ message: 'Accommodation deleted successfully' });
+    } catch (error) {
+        // Handle any errors that occur during the deletion process
+        res.status(500).json({ error: 'Failed to delete accommodation' });
+    }
+};
+
 module.exports = {
     createAccommodation,
     formatAccommodationForDisplay,
+    updateAccommodation,
+    deleteAccommodation,
 };
