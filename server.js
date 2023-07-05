689b29f3854774cfb15feed1e307caf404e37bc6
DeazStar <deadfromtheborn@gmail.com>
2023-07-05 11:19:25 +0300

    register the accomodation routes

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const routes = require('./routes/index');
+const accommodationRoutes = require('./routes/accommodationRoutes');
+
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use('/', routes);
+app.use('/accommodations', accommodationRoutes);
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
