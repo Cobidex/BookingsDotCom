9972128fb6844ff471f032ceb4186498f57c2eac
Stephen Obialor <stevobialor@gmail.com>
2023-07-05 22:48:56 +0000

    add route to user dashboard

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,7 @@ const UsersController = require('../controllers/UsersController');
 const routes = express.Router();
 
 routes.post('/users', UsersController.postNew);
+routes.post('/users/login', UsersController.loginUser);
+routes.get('/users/:id/dashboard', AuthController.verifyToken, UsersController.getProfile);
 
 module.exports = routes;
