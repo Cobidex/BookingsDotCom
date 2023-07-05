0e51a15eeee517896630cb5de8e77b01d9da32ed
DeazStar <deadfromtheborn@gmail.com>
2023-07-05 11:43:04 +0300

    register the updateAccomodation and deleteAccomodation

--- routes/accommodationRoutes.js
+++ routes/accommodationRoutes.js
@@ -4,5 +4,7 @@ const accommodationController = require('../controllers/accommodationController'
 
 
 router.post('/create', accommodationController.createAccommodation);
+router.put('/:id', accommodationController.updateAccommodation);
+router.delete('/:id', accommodationController.deleteAccommodation);
 
 module.exports = router;
