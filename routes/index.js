909ffe204b447a49cab4f9f5481b75fe42dac337
Stephen Obialor <stevobialor@gmail.com>
2023-07-06 20:57:59 +0000

    include new users routes

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,8 @@ const routes = express.Router();
 
 routes.post('/users', UsersController.postNew);
 routes.post('/users/login', UsersController.loginUser);
-routes.get('/users/:id/dashboard', AuthController.verifyToken, UsersController.getProfile);
+routes.get('/users', AuthController.verifyToken, UsersController.getUserProfile);
+routes.put('/users', AuthController.verifyToken, UsersController.putUser);
+routes.delete('/users', AuthController.verifyToken, UsersController.deleteUser);
 
 module.exports = routes;
