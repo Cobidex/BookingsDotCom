27b50f3ad3c0345ad4a782371593f5da57b895e5
Mohammed Tawfiq Iddrisu <84660782+Tawphiq@users.noreply.github.com>
2023-07-07 00:17:03 +0100

    "frontendComponents"

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <>
-      <div className='min-h-screen text-lg text-gray-500'>Hello everyone welcome to <span className='border-b-2 text-teal-500 shadow rounded-md bg-gray-50 text-2xl'>BookingsDotCom</span></div>
+      <div className='min-h-screen text-lg text-gray-500'>Hello everyone welcome to <span className='border-b-2 text-teal-500 shadow rounded-md text-2xl'>BookingsDotCom</span></div>
     </>
   )
 }
---
+++ frontend/src/components/AccomodationSearch.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AccomodationSearch = () => {
+  return (
+    <div>AccomodationSearch</div>
+  )
+}
+
+export default AccomodationSearch
