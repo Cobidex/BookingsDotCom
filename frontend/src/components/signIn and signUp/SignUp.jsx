5f97291bc9610ac32677979a3e7da1b5b176792b
Stephen Obialor <stevobialor@gmail.com>
2023-07-07 23:00:45 +0000

    add missing semi-colons, add firstname, lastname and phoneNumber fields

--- frontend/src/components/signIn and signUp/SignUp.jsx
+++ frontend/src/components/signIn and signUp/SignUp.jsx
@@ -1,15 +1,17 @@
-import React, { useState} from 'react'
-import { useHistory } from 'react-router-dom'
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 
 const SignUp = () => {
-  const [name, setName] = useState('')
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [error, setError] = useState('')
-  const [success, setSuccess] = useState('')
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [error, setError] = useState('');
+  const [success, setSuccess] = useState('');
 
   // access history object
-  const history = useHistory()
+  const history = useHistory();
 
   const handleSignUp = async (e) => {
     e.preventDefault()
@@ -21,29 +23,35 @@ const SignUp = () => {
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ name, email, password}),
-      })
+        body: JSON.stringify({
+	  firstName,
+	  lastName,
+	  email,
+	  password,
+	  phoneNumber
+	}),
+      });
 
       // here handles success response
       if (response.ok) {
         // display success message or redirect to the login page
-        setSuccess('Registration successful!')
-        setError('')
+        setSuccess('Registration successful!');
+        setError('');
 
         // redirect to log inn page after successful sign up
-        history.push('/login')
+        history.push('/login');
       } else {
         // display error message
-        const errorData = await response.json()
-        setError(errorData.message)
-        setSuccess('')
+        const errorData = await response.json();
+        setError(errorData.message);
+        setSuccess('');
       }
     } catch (error) {
       // handle network or other errors
-      setError('An error occurred. Please try again.')
-      setSuccess('')
+      setError('An error occurred. Please try again.');
+      setSuccess('');
     }
-  }
+  };
 
   return (
     <div>
@@ -52,14 +60,16 @@ const SignUp = () => {
       {success && <p className='text-green-500'>{success}</p>}
       <form onSubmit={handleSignUp}>
         {/* Form fields */}
-        <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+        <input type="text" value={firstName} onChange={(e) => setFirstName(e.target.value)} />
+	<input type="text" value={lastName} onChange={(e) => setLastName(e.target.value)} />
         <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
         <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+	<input type="text" value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} />
         {/* Submit button */}
         <button type="submit">Sign Up</button>
       </form>
     </div>
-  )
-}
+  );
+};
 
 export default SignUp
