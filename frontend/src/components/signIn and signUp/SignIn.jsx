b955b490a1926db213ee751f75b3aec0c01b65b5
Nnachi Kingsley <chimakingsley216@gmail.com>
2023-07-07 05:23:18 +0100

    modify sign in and sign up components

--- frontend/src/components/signIn and signUp/SignIn.jsx
+++ frontend/src/components/signIn and signUp/SignIn.jsx
@@ -1,8 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useHistory } from 'react-router-dom'
 
 const SignIn = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [error, setError] = useState('')
+
+  // access history object
+  const history = useHistory()
+
+  const handleSignIn = async (e) => {
+    e.preventDefault()
+
+    try {
+      const response = await fetch('your_authentication_endpoint', {
+        method: 'POST',
+        headers: {
+          'content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email, password }),
+      })
+
+      if (response.ok){
+        // redirect to dashboard after successful login
+        history.push('/dashboard')
+      } else {
+        const errorData = await response.json()
+        setError(errorData.message)
+      }
+    } catch (error) {
+      setError('An error occurred, please try again.')
+    }
+  }
+  
   return (
-    <div>SignIn</div>
+    <div>
+      <h2>Sign In</h2>
+      {error && <p className='text-red-500'>{error}</p>}
+      <form onSubmit={handleSignIn}>
+        {/* Form fields */}
+        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        {/* submit button */}
+        <button type='submit'>Sign In</button>
+      </form>
+    </div>
   )
 }
 
--- frontend/src/components/signIn and signUp/SignUp.jsx
+++ frontend/src/components/signIn and signUp/SignUp.jsx
@@ -1,8 +1,64 @@
-import React from 'react'
+import React, { useState} from 'react'
+import { useHistory } from 'react-router-dom'
 
 const SignUp = () => {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [error, setError] = useState('')
+  const [success, setSuccess] = useState('')
+
+  // access history object
+  const history = useHistory()
+
+  const handleSignUp = async (e) => {
+    e.preventDefault()
+
+    try {
+      // this make API call to register user using name, email and password
+      const response = await fetch('your_registration_endpoint', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ name, email, password}),
+      })
+
+      // here handles success response
+      if (response.ok) {
+        // display success message or redirect to the login page
+        setSuccess('Registration successful!')
+        setError('')
+
+        // redirect to log inn page after successful sign up
+        history.push('/login')
+      } else {
+        // display error message
+        const errorData = await response.json()
+        setError(errorData.message)
+        setSuccess('')
+      }
+    } catch (error) {
+      // handle network or other errors
+      setError('An error occurred. Please try again.')
+      setSuccess('')
+    }
+  }
+
   return (
-    <div>SignUp</div>
+    <div>
+      <h2>SignUp</h2>
+      {error && <p className='text-red-500'>{error}</p>}
+      {success && <p className='text-green-500'>{success}</p>}
+      <form onSubmit={handleSignUp}>
+        {/* Form fields */}
+        <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        {/* Submit button */}
+        <button type="submit">Sign Up</button>
+      </form>
+    </div>
   )
 }
 
