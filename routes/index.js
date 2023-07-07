453c8abf465ed7c6752c0a08e1542c24a3c3f831
Stephen Obialor <stevobialor@gmail.com>
2023-07-07 20:00:47 +0000

    modify routes to include admin permissions

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,8 @@ const routes = express.Router();
 
 routes.post('/users', UsersController.postNew);
 routes.post('/users/login', UsersController.loginUser);
-routes.get('/users', AuthController.verifyToken, UsersController.getUserProfile);
-routes.put('/users', AuthController.verifyToken, UsersController.putUser);
-routes.delete('/users', AuthController.verifyToken, UsersController.deleteUser);
+routes.get('/users/:userId', AuthController.verifyToken, UsersController.getUserProfile);
+routes.put('/users/:userId', AuthController.verifyToken, UsersController.putUser);
+routes.delete('/users/:userId', AuthController.verifyToken, UsersController.deleteUser);
 
 module.exports = routes;
