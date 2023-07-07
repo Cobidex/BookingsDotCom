70c4882b9f59ac74aa1f63958ab35d79c829a67c
Mohammed Tawfiq Iddrisu <84660782+Tawphiq@users.noreply.github.com>
2023-07-07 01:02:30 +0100

    "putting it in its rightful folder"

--- frontend/src/components/AccomodationSearch.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const AccomodationSearch = () => {
-  return (
-    <div>AccomodationSearch</div>
-  )
-}
-
-export default AccomodationSearch
---
+++ frontend/src/components/booking/BookingDetails.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BookingDetails = () => {
+  return (
+    <div>BookingDetails</div>
+  )
+}
+
+export default BookingDetails
