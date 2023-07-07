18125bf29a382c2b873790d2e133c4b462a69195
Nnachi Kingsley <chimakingsley216@gmail.com>
2023-07-07 14:33:39 +0100

    create accommodation components

--- frontend/src/components/Search/SearchForm.jsx
+++ frontend/src/components/Search/SearchForm.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const SearchForm = () => {
+const SearchForm = ({ onSearch }) => {
+  const [location, setLocation] = useState('')
+  const [accommodationType, setAccommodationType] = useState('')
+  const [priceRange, setPriceRange] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    // call the onSearch function from the parent component with the search critria
+    onSearch({ location, accommodationType, priceRange })
+  }
+
   return (
-    <div>SearchForm</div>
+    <div>
+      {/* <h2>SearchForm</h2> */}
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={location} onChange={(e) => setLocation(e.target.value)} />
+        <input type="text" value={accommodationType} onChange={(e) => setAccommodationType(e.target.value)}/>
+        <input type="text" value={priceRange} onChange={(e) => setPriceRange(e.target.value)} />
+        <button type='submit'>Search</button>
+      </form>
+    </div>
   )
 }
 
--- frontend/src/components/Search/SearchResults.jsx
+++ frontend/src/components/Search/SearchResults.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const SearchResults = () => {
+const SearchResults = ({ results }) => {
   return (
-    <div>SearchResults</div>
+    <div>
+      {/* <h2>Search Results</h2>*/}
+      {results.length === 0 ? (
+        <p>No Result Found.</p>
+      ) : (
+        <ul>
+          {results.map((result) => (
+            <li key={result.id}>
+              {/* Display details of all Search Results */}
+              <h3>{result.name}</h3>
+              <p>Location: {result.location}</p>
+              <p>Type: {result.type}</p>
+              <p>Price: {result.price}</p>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
   )
 }
 
