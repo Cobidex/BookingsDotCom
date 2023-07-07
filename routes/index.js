82527af46ddc0a5166911454c2a10b00a45c8a42
Stephen Obialor <stevobialor@gmail.com>
2023-07-07 21:02:37 +0000

    include accomodations route

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const UsersController = require('../controllers/UsersController');
+const { 
+  updateAccommodation,
+  deleteAccommodation,
+  createAccommodation,
+  searchAccommodations,
+} = require('../controllers/accommodationController');
 
 const routes = express.Router();
 
 routes.post('/users', UsersController.postNew);
 routes.post('/users/login', UsersController.loginUser);
-routes.get('/users/:userId', AuthController.verifyToken, UsersController.getUserProfile);
-routes.put('/users/:userId', AuthController.verifyToken, UsersController.putUser);
-routes.delete('/users/:userId', AuthController.verifyToken, UsersController.deleteUser);
-
+routes.get('/users/:id', AuthController.verifyToken, UsersController.getUserProfile);
+routes.put('/users/:id', AuthController.verifyToken, UsersController.putUser);
+routes.delete('/users/:id', AuthController.verifyToken, UsersController.deleteUser);
+routes.put('/accommodations/:id', AuthController.verifyToken, updateAccommodation);
+routes.delete('/accommodations/:id', AuthController.verifyToken, deleteAccommodation);
+routes.get('/accommodations', searchAccommodations);
+routes.post('/accommodations/:city_id/accommodations', AuthController.verifyToken, createAccommodation);
 module.exports = routes;
