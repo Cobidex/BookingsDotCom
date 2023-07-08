073a99766dbef90eeea6740f164ce4a877329453
Stephen Obialor <stevobialor@gmail.com>
2023-07-08 12:22:12 +0000

    improve user registration implementation

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -26,46 +26,35 @@ async function comparePasswords(plainPassword, hashedPassword) {
 class UsersController {
   static async postNew(req, res) {
     const { firstName, lastName, email, password, phoneNumber, isAdmin } = req.body;
-    if (!firstName) {
-      return res.status(400).send({ error: 'Missing firstName' });
+    if (!firstName || !lastName || !email || !password || !phoneNumber) {
+      return res.status(400).send({ error: 'Missing required fields' });
     }
-    if (!lastName) {
-      return res.status(400).send({ error: 'Missing lastName' });
-    }
-    if (!email) {
-      return res.status(400).send({ error: 'Missing email' });
-    }
-    if (!password) {
-      return res.status(400).send({ error: 'Missing password' });
-    }
-    if (!phoneNumber) {
-      return res.status(400).send({ error: 'Missing phoneNumber' });
-    }
-    const user = await User.findOne({ where: { email } });
-    if (user) {
-      return res.status(400).send({ error: 'Email Already exists' });
-    }
-
-    const hash = await hashPassword(password);
-    if (!hash) {
-      return res.status(501).send({ error: 'internal Server error' });
-    }
-
-    const newUser = {
-      firstName,
-      lastName,
-      email,
-      password: hash,
-      phoneNumber,
-      isAdmin
-    };
 
     try {
+      const user = await User.findOne({ where: { email } });
+      if (user) {
+        return res.status(400).send({ error: 'Email Already exists' });
+      }
+
+      const hash = await hashPassword(password);
+      if (!hash) {
+        return res.status(501).send({ error: 'internal Server error' });
+      }
+
+      const newUser = {
+        firstName,
+        lastName,
+        email,
+        password: hash,
+        phoneNumber,
+        isAdmin
+      };
+
       const createdUser = await User.create(newUser);
       return res.status(200).send({ Name: createdUser.getName(), id: createdUser.id });
     } catch (error) {
       console.log('error writing to database', error);
-      return null
+      return res.status(500).send({ error: 'Internal server error' });
     }
   }
 
