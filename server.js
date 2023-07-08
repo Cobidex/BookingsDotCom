c73499555bf7109adf505b5cba103cc0a87269e1
Stephen Obialor <stevobialor@gmail.com>
2023-07-08 12:22:57 +0000

    modify user route initials

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const routes = require('./routes/index');
+const userRoutes = require('./routes/userRoutes');
 const accommodationRoutes = require('./routes/accommodationRoutes');
 const bookingRoutes = require('./routes/bookingRoutes');
 
@@ -9,7 +9,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use('/', routes);
+app.use('/users', userRoutes);
 app.use('/accommodations', accommodationRoutes);
 app.use('/booking', bookingRoutes);
 
