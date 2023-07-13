f9b5ffa431467ce8e94bd21fbd7c49b9e9fbb9eb
Mohammed Tawfiq Iddrisu <84660782+Tawphiq@users.noreply.github.com>
2023-07-13 22:58:14 +0100

    "adding more styling to the signIn component"

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ import SignIn from './components/signIn and signUp/SignIn'
 const App = () => {
   return (
     <>
-      <div className='text-lg text-gray-500'>Hello everyone welcome to <span className='border-b-2 text-teal-500 shadow rounded-md text-2xl'>BookingsDotCom</span></div>
+      {/*<div className='text-lg text-gray-500'>Hello everyone welcome to <span className='border-b-2 text-teal-500 shadow rounded-md text-2xl'>BookingsDotCom</span></div>*/}
       <SignIn />
     </>
   )
