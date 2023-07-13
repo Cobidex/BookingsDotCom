31bd21356577ce09d295972174331a8d6f4ce287
DeazStar <deadfromtheborn@gmail.com>
2023-07-13 11:06:09 +0300

    fix the CORS problem

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,6 @@ import accommodationRoutes from './routes/accommodationRoutes.js';
 import bookingRoutes from './routes/bookingRoutes.js';
 import reviewRoutes from './routes/reviewRoutes.js';
 
-
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -15,10 +14,11 @@ app.use(
   cors({
     origin: 'http://localhost:5173',
     optionsSuccessStatus: 200,
-    allowedHeaders: '*',
+    allowedHeaders: 'Content-Type, Authorization',
     credentials: true,
   })
 );
+
 app.use(express.json());
 app.use(cookieParser());
 app.use('/users', userRoutes);
@@ -26,6 +26,12 @@ app.use('/accommodations', accommodationRoutes);
 app.use('/booking', bookingRoutes);
 app.use('/cities', cityRoutes);
 app.use('/reviews', reviewRoutes);
+
+// Handle OPTIONS request
+app.options('*', (req, res) => {
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.sendStatus(200);
+});
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
