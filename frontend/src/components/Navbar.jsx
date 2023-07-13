73f4dfd57ad9d36a4e4b4ac7e7e650857a493735
Mohammed Tawfiq Iddrisu <84660782+Tawphiq@users.noreply.github.com>
2023-07-14 00:21:32 +0100

    "styling the logo"

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,8 +4,8 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div className='grid grid-cols-2 mt-5 mb-5 gap-4'>
-      <div className='grid text-2xl items-center cursor-pointer bg-gray-100 w-48 text-teal-500 shadow-md mb-2 rounded-md border-b-4 border-teal-500 p-2 ml-5'>BookingDotCom</div>
-      <ul className='grid grid-cols-4 text-teal-500'>
+      <div className='grid text-2xl items-start cursor-pointer bg-gray-100 w-48 text-teal-500 shadow-md rounded-md border-b-2 border-teal-500 p-2 ml-5'>BookingDotCom</div>
+      <ul className='grid grid-cols-4 mt-4 text-teal-500'>
         <li><a href="" className='nav'>Home</a></li>
         <li><a href="" className='nav'>Accomodation</a></li>
         <li><a href="" className='nav'>Booking</a></li>
