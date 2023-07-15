64098ea986ed9386cd73138f461317466d883600
Stephen Obialor <stevobialor@gmail.com>
2023-07-15 12:17:59 +0000

    modify methods

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -6,7 +6,7 @@ const userRoutes = express.Router();
 
 userRoutes.post('/signup', UsersController.signUp);
 userRoutes.post('/signin', UsersController.signIn);
-userRoutes.post('/profile', AuthController.verifyUser, UsersController.getUserProfile);
+userRoutes.get('/profile', AuthController.verifyUser, UsersController.getUserProfile);
 userRoutes.get('/count', AuthController.verifyAdmin, UsersController.getUserCount);
 userRoutes.put('/profile', AuthController.verifyUser, UsersController.editUserProfile);
 userRoutes.delete('/delete', AuthController.verifyUser, UsersController.deleteUser);
