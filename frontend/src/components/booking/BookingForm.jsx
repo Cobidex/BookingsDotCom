68f83eaeff346a14819506ffde8c8320dccce4cc
Mohammed Tawfiq Iddrisu <84660782+Tawphiq@users.noreply.github.com>
2023-07-15 01:01:46 +0100

    "Added styling to the ReviewForm"

--- frontend/src/components/booking/BookingForm.jsx
+++ frontend/src/components/booking/BookingForm.jsx
@@ -34,44 +34,45 @@ const BookingForm = () => {
   return (
     <div className="flex flex-col justify-center items-center min-h-screen bg-white">
       <div className="max-w-md w-full mx-4 bg-gray-50 p-6 rounded-lg shadow-md border border-teal-500">
+        <p className='text-center text-2xl font-semibold text-gray-500 mb-4'>Book Accomodation</p>
         <p className="text-gray-700 mb-4 font-thin text-lg">
         Discover Your Ideal Getaway: Book Your Stay Now and Create Lasting Memories
         </p>
         <form onSubmit={handleFormSubmit}>
           <div className="mb-4">
-            <label htmlFor="checkIn" className="block text-gray-700 ">
+            <label htmlFor="checkIn" className="block text-gray-700 text-xl font-thin">
               Check-in:
             </label>
             <input
               type="date"
               id="checkIn"
-              className="w-full rounded-md px-3 py-2 focus:ring-teal-500 focus:border-teal-500 border-teal-500 shadow"
+              className="w-full border text-gray-700 font-thin rounded-md px-3 py-2 focus:ring-teal-500 focus:border-teal-500 border-teal-500 shadow"
               value={checkIn}
               onChange={(e) => setCheckIn(e.target.value)}
               required
             />
           </div>
           <div className="mb-4">
-            <label htmlFor="checkOut" className="block text-gray-700">
+            <label htmlFor="checkOut" className="block text-gray-700 text-xl font-thin">
               Check-out:
             </label>
             <input
               type="date"
               id="checkOut"
-              className="w-full border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring-teal-500 focus:border-teal-500 shadow"
+              className="w-full text-gray-700 font-thin border border-teal-500 rounded-md px-3 py-2 focus:outline-none focus:ring-teal-500 focus:border-teal-500 shadow"
               value={checkOut}
               onChange={(e) => setCheckOut(e.target.value)}
               required
             />
           </div>
           <div className="mb-4">
-            <label htmlFor="accommodationId" className="block text-gray-700">
+            <label htmlFor="accommodationId" className="block text-gray-700 text-xl font-thin">
               Accommodation ID:
             </label>
             <input
               type="text"
               id="accommodationId"
-              className="w-full border-gray-300 rounded-md px-3 py-2 focus:outline-none shadow placeholder:font-light"
+              className="w-full rounded-md px-3 py-2 focus:outline-none shadow placeholder:font-light text-lg text-gray-700 font-thin border border-teal-500"
               value={accommodationId}
               onChange={(e) => setAccommodationId(e.target.value)}
               placeholder='Enter accommodation ID'
@@ -81,7 +82,7 @@ const BookingForm = () => {
           {errorMessage && <p className="text-red-500 mb-4">{errorMessage}</p>}
           <button
             type="submit"
-            className="w-full bg-teal-500 hover:bg-teal-700 text-white rounded-md py-2 px-4"
+            className="w-full inline-flex justify-center px-4 py-2 text-sm font-medium text-white shadow border bg-teal-500 hover:border-teal-500 hover:bg-white hover:text-teal-500 focus:outline-none focus:ring-2 rounded-md mt-10 mb-5"
           >
             Book
           </button>
--- frontend/src/components/review/ReviewForm.jsx
+++ frontend/src/components/review/ReviewForm.jsx
@@ -33,10 +33,23 @@ const ReviewForm = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <div className="flex flex-col justify-center items-center min-h-screen bg-white">
+     <div className="max-w-md w-full mx-4 bg-gray-50 p-6 rounded-lg shadow-md border border-teal-500">
       <div>
-        <label htmlFor="rating">Rating:</label>
-        <select id="rating" value={rating} onChange={handleRatingChange} required>
+        <p className='text-center text-2xl font-semibold text-gray-500 mb-4'>Leave a review</p>
+        <p className='mb-4 text-gray-700 font-thin text-lg'>Share your experience and help others make informed decisions. Rate the accommodation on a scale of 1 to 5 and provide additional feedback in the comments.
+           Your valuable review will contribute to a better community of travelers. Thank you for your input!</p>
+      </div> 
+    <form onSubmit={handleSubmit} className="max-w-sm mx-auto">
+      <div className="mb-4">
+        <label htmlFor="rating" className="text-gray-700 text-xl font-thin">Rating:</label>
+        <select
+          id="rating"
+          value={rating}
+          onChange={handleRatingChange}
+          required
+          className="block w-full mt-1 px-2 py-1 border border-teal-500 focus:outline-none focus:ring-teal-500 focus:border-teal-500 sm:text-sm rounded-md shadow"
+        >
           <option value="">Select a rating</option>
           <option value="1">1</option>
           <option value="2">2</option>
@@ -45,12 +58,25 @@ const ReviewForm = () => {
           <option value="5">5</option>
         </select>
       </div>
-      <div>
-        <label htmlFor="text">Review Text:</label>
-        <textarea id="text" value={text} onChange={handleTextChange} required></textarea>
+      <div className="mb-4">
+        <label htmlFor="text" className="text-gray-700 text-xl font-thin">Review Text:</label>
+        <textarea
+          id="text"
+          value={text}
+          onChange={handleTextChange}
+          required
+          className="block w-full mt-1 px-2 py-1 border border-teal-500 shadow focus:outline-none focus:ring-teal-500 focus:border-teal-500 sm:text-sm rounded-md text-gray-700 text-lg font-thin"
+        ></textarea>
       </div>
-      <button type="submit">Submit Review</button>
+      <button
+        type="submit"
+        className="w-full inline-flex justify-center px-4 py-2 text-sm font-medium text-white shadow border bg-teal-500 hover:border-teal-500 hover:bg-white hover:text-teal-500 focus:outline-none focus:ring-2 rounded-md mt-10 mb-5"
+      >
+        Submit Review
+      </button>
     </form>
+    </div>
+    </div>
   );
 };
 
