118910738cf824fb1a07dd81a2bbf68a3c1bc4b4
Tawphiq <mohammedtawfiqiddrisu5@gmail.com>
2023-07-18 23:54:27 +0000

    no commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,12 +16,13 @@ import ReviewList from './components/review/ReviewList'
 //Dashboard
 import UserDashboard from './components/dashboard/UserDashboard'
 import BookingHistory from './components/dashboard/BookingHistory'
+import Footer from './components/dashboard/BookingHistory'
 
 
 const App = () => {
   return (
     <>
-      <div className='bg-gray-100 min-h-screen'>
+      <div className='bg-[url("https://drive.google.com/file/d/1PG5AzdF16nRx2p9W0g_xnEVhXrulzARb/view?usp=sharing")] bg-cover min-h-screen'>
       {/*<div className='text-lg text-gray-500'>Hello everyone welcome to <span className='border-b-2 text-teal-500 shadow rounded-md text-2xl'>BookingsDotCom</span></div>*/}
       {/* <SignIn /> */}
       {/* <SignUp /> */}
@@ -38,6 +39,7 @@ const App = () => {
           <Route path='/ReviewList' element={<ReviewList />} />
           <Route path='/UserDashboard' element={<UserDashboard />} />
           <Route path='/BookingHistory' element={<BookingHistory />} />
+	  <Route path='/Footer' element={<Footer />} />
         </Routes>
         </div>
     </>
--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
 
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
+	  return (
+		      <div className="relative">
+		        <video
+		          className="w-full h-auto absolute top-0 left-0 z-0"
+		          autoPlay
+		          muted
+		          loop
+		        >
+		          <source src="https://drive.google.com/uc?export=download&id=1PG5AzdF16nRx2p9W0g_xnEVhXrulzARb" type="video/mp4" />
+		          Your browser does not support the video tag.
+		        </video>
 
-export default Footer
+		        {/* Your other content goes here */}
+		      </div>
+		    );
+};
+
+export default Footer;
+
