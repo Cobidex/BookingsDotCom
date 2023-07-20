2d30df2d15b1aad38b1dda173392f801f03bc2c7
Mohammed Tawfiq Iddrisu <84660782+Tawphiq@users.noreply.github.com>
2023-07-21 00:17:05 +0100

    "modifications to the search results"

--- frontend/src/components/Search/SearchResults.jsx
+++ frontend/src/components/Search/SearchResults.jsx
@@ -68,7 +68,7 @@ const SearchResults = ({ results }) => {
     <div>
       {/*<h2>Search Results</h2>*/}
       {results.length === 0 ? (
-        <p>No results found.</p>
+        <p></p>
       ) : (
         <ul>
           {results.map((accommodation) => (
