2be83528358fb01a28a82905bfe69a4cfb951257
Mohammed Tawfiq Iddrisu <84660782+Tawphiq@users.noreply.github.com>
2023-07-21 03:09:04 +0100

    "text white"

--- frontend/src/components/Search/SearchResults.jsx
+++ frontend/src/components/Search/SearchResults.jsx
@@ -66,7 +66,7 @@ import { Link } from 'react-router-dom';
 
 const SearchResults = ({ results }) => {
   return (
-    <div>
+    <div className='text-white'>
       {/*<h2>Search Results</h2>*/}
       {results.length === 0 ? (
         <p></p>
